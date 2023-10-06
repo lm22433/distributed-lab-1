a2068e4a44855d0612666ec0d9e7a8914d876f0c
Harry Greentree <harrygreentree@icloud.com>
2023-10-06 14:47:01 +0100

    Question 2: Implement Client-Chat Server

--- client/client.go
+++ client/client.go
@@ -1,24 +1,50 @@
 package main
 
 import (
+	"bufio"
 	"flag"
-	"net"
 	"fmt"
+	"net"
+	"os"
 )
 
 func read(conn net.Conn) {
-	//TODO In a continuous loop, read a message from the server and display it.
+	for {
+		msg, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading from server:", err)
+		}
+		fmt.Print(msg)
+	}
 }
 
 func write(conn net.Conn) {
-	//TODO Continually get input from the user and send messages to the server.
+	for {
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		msg := scanner.Text()
+		_, err := fmt.Fprintln(conn, msg)
+		if err != nil {
+			fmt.Println("Error sending message:", err)
+		}
+	}
 }
 
 func main() {
 	// Get the server address and port from the commandline arguments.
 	addrPtr := flag.String("ip", "127.0.0.1:8030", "IP:port string to connect to")
 	flag.Parse()
-	//TODO Try to connect to the server
-	//TODO Start asynchronously reading and displaying messages
-	//TODO Start getting and sending user messages.
+
+	// Attempt to connect to the server and handle the error.
+	conn, err := net.Dial("tcp", *addrPtr)
+	if err != nil {
+		fmt.Println("Error connecting to the server:", err)
+	}
+
+	// Asynchronously start both the read and write goroutines.
+	go read(conn)
+	go write(conn)
+
+	for {
+	}
 }
--- server/server.go
+++ server/server.go
@@ -3,8 +3,8 @@ package main
 import (
 	"bufio"
 	"flag"
-	"net"
 	"fmt"
+	"net"
 )
 
 type Message struct {
@@ -12,23 +12,24 @@ type Message struct {
 	message string
 }
 
-func handleError(err error) {
-	// TODO: all
-	// Deal with an error event.
-}
-
 func acceptConns(ln net.Listener, conns chan net.Conn) {
-	// TODO: all
 	// Continuously accept a network connection from the Listener
 	// and add it to the channel for handling connections.
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+		}
+		conns <- conn
+	}
 }
 
 func handleClient(client net.Conn, clientid int, msgs chan Message) {
-	// TODO: all
-	// So long as this connection is alive:
-	// Read in new messages as delimited by '\n's
-	// Tidy up each message and add it to the messages channel,
-	// recording which client it came from.
+	scanner := bufio.NewScanner(client)
+	for scanner.Scan() {
+		message := scanner.Text()
+		msgs <- Message{sender: clientid, message: message}
+	}
 }
 
 func main() {
@@ -37,7 +38,12 @@ func main() {
 	portPtr := flag.String("port", ":8030", "port to listen on")
 	flag.Parse()
 
-	//TODO Create a Listener for TCP connections on the port given above.
+	// Create a listener for TCP connections on the given port
+	ln, err := net.Listen("tcp", *portPtr)
+	if err != nil {
+		fmt.Println("Error creating listener:", err)
+		return
+	}
 
 	//Create a channel for connections
 	conns := make(chan net.Conn)
@@ -48,16 +54,27 @@ func main() {
 
 	//Start accepting connections
 	go acceptConns(ln, conns)
+
+	clientId := 0
 	for {
 		select {
 		case conn := <-conns:
-			//TODO Deal with a new connection
-			// - assign a client ID
-			// - add the client to the clients channel
-			// - start to asynchronously handle messages from this client
+			// Assign a client ID
+			clients[clientId] = conn
+			// Start handling messages from this client
+			go handleClient(conn, clientId, msgs)
+			// Increment client id
+			clientId++
 		case msg := <-msgs:
-			//TODO Deal with a new message
-			// Send the message to all clients that aren't the sender
+			for id, conn := range clients {
+				// Ignore the sender
+				if id != msg.sender {
+					_, err = fmt.Fprintln(conn, fmt.Sprintf("Client %d: %s", msg.sender, msg.message))
+					if err != nil {
+						fmt.Println("Error sending message:", err)
+					}
+				}
+			}
 		}
 	}
 }
