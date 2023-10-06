61c280f67cd06b9c0e26202ffa78a1f555093ae8
Harry Greentree <harrygreentree@icloud.com>
2023-10-06 13:21:09 +0100

    Question 1: Stage 2

--- simple-client-server/server.go
+++ simple-client-server/server.go
@@ -6,10 +6,16 @@ import (
 	"net"
 )
 
-func main() {
-	ln, _ := net.Listen("tcp", "127.0.0.1:8030")
-	conn, _ := ln.Accept()
+func handleConnection(conn net.Conn) {
 	reader := bufio.NewReader(conn)
 	msg, _ := reader.ReadString('\n')
 	fmt.Println(msg)
+}
+
+func main() {
+	ln, _ := net.Listen("tcp", "127.0.0.1:8030")
+	for {
+		conn, _ := ln.Accept()
+		go handleConnection(conn)
+	}
 }
