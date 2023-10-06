e41ff54789ed04f33296df754ac29aced526cd46
Harry Greentree <harrygreentree@icloud.com>
2023-10-06 13:22:55 +0100

    Question 1: Stage 3

--- simple-client-server/client.go
+++ simple-client-server/client.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"os"
 )
 
 func main() {
-	msg := "Hello from Server!"
-	conn, _ := net.Dial("tcp", "127.0.0.1:8030")
+	// Prompt the user for a message
+	fmt.Print("Enter the message: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	msg := scanner.Text()
+
+	conn, err := net.Dial("tcp", "127.0.0.1:8030")
+	if err != nil {
+		fmt.Println("Error connecting to the server:", err)
+		return
+	}
+	defer conn.Close()
+
 	fmt.Fprintln(conn, msg)
 }
